f7e0ef6d6352567438b8b6551e512ea7852d58a2
Andrey <andrey.sela.995@gmail.com>
2021-01-02 22:14:08 +0200

    Added a new class for testing

---
+++ SHOPExercise/Data/GitTesr.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SHOPExercise.Data
+{
+    public class GitTesr
+    {
+        // added for test a git
+    }
+}
